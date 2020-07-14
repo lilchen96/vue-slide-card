106a519bd60fcd4e228ce27d27d07ed996ff6dc7
lilchen96 <lilchen96@outlook.com>
2020-07-14 23:47:41 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <test>
       <template slot="content">13231331332</template>
-      <template slot="operation">2·21·31231</template>
+      <template slot="operation">2·21·312的点点滴滴31</template>
     </test>
   </div>
 </template>
